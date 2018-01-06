ab6c167ae3b50c4189f622a6b7ddc5c87766cb84
DHaylock <david.haylock@watershed.co.uk>
2018-01-06 00:16:21 +0000

    - Initial Commit for iOS App

---
+++ OhEuropa/OhEuropa/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OhEuropa
+//
+//  Created by David Haylock on 06/01/2018.
+//  Copyright © 2018 David Haylock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

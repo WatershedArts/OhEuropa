3bf66fad547865d2f70537e33c43a5a9d78d590e
DHaylock <david.haylock@watershed.co.uk>
2018-01-10 11:19:46 +0000

    -

--- OhEuropa/OhEuropa/OEMapViewController.swift
+++ OhEuropa/OhEuropa/OEMapViewController.swift
@@ -30,52 +30,6 @@ class OEMapViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-	///------------------------------------------------------------------------------------------
-	/// Load The View
-	///------------------------------------------------------------------------------------------
-	override func loadView() {
-		determineMyCurrentLocation()
-		
-		let camera = GMSCameraPosition.camera(withLatitude: 51.4545404, longitude: -2.6081, zoom: 14)
-		mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-		mapView.isMyLocationEnabled = true
-		self.view = mapView
-		var a = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-		a.setTitle("Back", for: UIControlState.normal)
-		a.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
-		
-		self.view.addSubview(a)
-		
-		for beacon in beacons {
-			
-			print(beacon.name)
-			
-			// This is for Debug purposes only
-			let outerCircle = GMSCircle(position: beacon.centerCoordinate, radius: CLLocationDistance(beacon.radius*3))
-			outerCircle.title = beacon.name
-			outerCircle.strokeColor = UIColor.red
-			outerCircle.fillColor = UIColor.red
-			outerCircle.isTappable = true
-			outerCircle.map = mapView
-			
-			let midCircle = GMSCircle(position: beacon.centerCoordinate, radius: CLLocationDistance(beacon.radius*2))
-			midCircle.title = beacon.name
-			midCircle.strokeColor = UIColor.orange
-			midCircle.fillColor = UIColor.orange
-			midCircle.isTappable = true
-			midCircle.map = mapView
-			
-			let innerCirle = GMSCircle(position: beacon.centerCoordinate, radius: CLLocationDistance(beacon.radius))
-			innerCirle.title = beacon.name
-			innerCirle.strokeColor = UIColor.green
-			innerCirle.fillColor = UIColor.green
-			innerCirle.isTappable = true
-			innerCirle.map = mapView
-		}
-		
-		
-	}
 	
 	@objc func dismissViewController() {
 		print("Dismissing Popover")

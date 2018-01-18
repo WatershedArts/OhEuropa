b7a64afda41ce856398c4e336ce74532f1bd2efe
DHaylock <david.haylock@watershed.co.uk>
2018-01-18 23:24:05 +0000

    - Removed References to Macaw

--- OhEuropa/OhEuropa/Models/OEBeaconMarker.swift
+++ OhEuropa/OhEuropa/Models/OEBeaconMarker.swift
@@ -7,11 +7,10 @@
 //
 
 import UIKit
-import Macaw
+//import Macaw
 
-class OEBeaconMarker: Group {
+class OEBeaconMarker {
 	var currentAngle: Double = 90.0
-	var col:Color!
 	var radius:Double!
 	var x:Double!
 	var y:Double!
@@ -23,24 +22,23 @@ class OEBeaconMarker: Group {
 	/// - Parameter beaconName:
 	///------------------------------------------------------------------------------------------
 	init(beaconName:String,x:Double,y:Double,radius:Double,name:String) {
-		col = Color.rgb(r: Int(arc4random_uniform(255)), g:Int(arc4random_uniform(255)), b: Int(arc4random_uniform(255)))
 		
 		self.x = x
 		self.y = y
 		
 		self.name = name
 //		let shape = Shape(form: Circle(cx:x,cy:y,r:10), fill: col, stroke: Stroke(fill: col, width: 3))
-		let shape = Shape(form:
-						  Polygon(points: [
-							self.x,self.y-15.0,
-							self.x+7.5,self.y+7.5,
-							self.x-7.5,self.y+7.5
-						  ]),
-						  fill: col,
-						  stroke: Stroke(fill: col, width: 1))
-		
-		self.radius = radius
-		super.init(contents: [shape])
+//		let shape = Shape(form:
+//						  Polygon(points: [
+//							self.x,self.y-15.0,
+//							self.x+7.5,self.y+7.5,
+//							self.x-7.5,self.y+7.5
+//						  ]),
+//						  fill: col,
+//						  stroke: Stroke(fill: col, width: 1))
+//
+//		self.radius = radius
+//		super.init(contents: [shape])
 		
 //		self.placeVar.animate(from: Transform.rotate(angle: 0.0.toRadians(),x:x,y:y), to: Transform.rotate(angle: 90.0.toRadians(),x:x,y:y), during: 0.5, delay: 0.0)
 		
@@ -51,30 +49,30 @@ class OEBeaconMarker: Group {
 	}
 	
 	///------------------------------------------------------------------------------------------
-	/// Set where beacon marker heading
-	///
-	/// - Parameter heading: Heading in relation to current location
-	///------------------------------------------------------------------------------------------
-	func setBeaconHeading(heading:Double) {
-		let originX = 0.0
-		let originY = 0.0
-
-		let rx = originX + (radius * sin(heading))
-		let ry = originY - (radius * cos(heading))
-		
-        let tmpAngle = (heading).toRadians()
-		
-//		self.placeVar.animate(from: Transform.rotate(angle: currentAngle, x:x, y:y+radius),
-//							  to: Transform.rotate(angle: tmpAngle ,x:x, y:y+radius),
-//							  during: 0.5,
-//							  delay: 0.0)
-		self.place = Transform.move(dx: rx, dy: ry)
-		
-		currentAngle = tmpAngle
-//		print("Beacon Name \(name) Angle: \(currentAngle)")
-	}
+//	/// Set where beacon marker heading
+//	///
+//	/// - Parameter heading: Heading in relation to current location
+//	///------------------------------------------------------------------------------------------
+//	func setBeaconHeading(heading:Double) {
+//		let originX = 0.0
+//		let originY = 0.0
+//
+//		let rx = originX + (radius * sin(heading))
+//		let ry = originY - (radius * cos(heading))
+//
+//        let tmpAngle = (heading).toRadians()
+//
+////		self.placeVar.animate(from: Transform.rotate(angle: currentAngle, x:x, y:y+radius),
+////							  to: Transform.rotate(angle: tmpAngle ,x:x, y:y+radius),
+////							  during: 0.5,
+////							  delay: 0.0)
+//		self.place = Transform.move(dx: rx, dy: ry)
+//
+//		currentAngle = tmpAngle
+////		print("Beacon Name \(name) Angle: \(currentAngle)")
+//	}
 	
-	func returnColor() -> Color {
-		return col
-	}
+//	func returnColor() -> Color {
+//		return col
+//	}
 }

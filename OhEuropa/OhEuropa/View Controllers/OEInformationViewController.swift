5f9c5fe3d46a5e34305b708c95e7738e59d2401e
DHaylock <david.haylock@watershed.co.uk>
2018-02-01 15:20:28 +0000

    - Removed Back button and added Gradient View

--- OhEuropa/OhEuropa/View Controllers/OEInformationViewController.swift
+++ OhEuropa/OhEuropa/View Controllers/OEInformationViewController.swift
@@ -12,44 +12,33 @@ import FontAwesome_swift
 
 class OEInformationViewController: UIViewController {
 
+	var gradientLayer: CAGradientLayer!
 	
+	///------------------------------------------------------------------------------------------
+	/// Create the Gradient for the Background
+	///------------------------------------------------------------------------------------------
+	func createGradientForBackground() {
+		gradientLayer = CAGradientLayer()
+		gradientLayer.colors = [DEFAULT_COLOR_OPPOSED.cgColor,DEFAULT_COLOR.cgColor]
+		gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+		gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+		gradientLayer.frame = self.view.frame
+		self.view.layer.insertSublayer(gradientLayer,at: 0)
+	}
+	
+	///------------------------------------------------------------------------------------------
+    /// View Did Load
+	///------------------------------------------------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-		
+		createGradientForBackground()
     }
 	
-	override func viewDidLayoutSubviews() {
-		print(self.view.bounds)
-		let backButton = UIButton(frame: CGRect(x: self.view.bounds.width-(14+40), y: self.view.bounds.height-(14+40), width: 40, height: 40))
-		backButton.setImage(UIImage.fontAwesomeIcon(name: .compass, textColor: DEFAULT_COLOR_OPPOSED, size: CGSize(width: 25, height: 25), backgroundColor: UIColor.clear), for: UIControlState.normal)
-		
-		backButton.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
-		backButton.layer.cornerRadius = 0.5 * backButton.bounds.size.width
-		backButton.backgroundColor = DEFAULT_COLOR
-		self.view.addSubview(backButton)
-	}
-
+	///------------------------------------------------------------------------------------------
+	/// Memory
+	///------------------------------------------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-	///------------------------------------------------------------------------------------------
-	/// Dismiss the View Controller
-	///------------------------------------------------------------------------------------------
-	@objc func dismissViewController() {
-		print("Dismissing Popover")
-		self.dismiss(animated: true, completion: nil)
-	}
-	
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }

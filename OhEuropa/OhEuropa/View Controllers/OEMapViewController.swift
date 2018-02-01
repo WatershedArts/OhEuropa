a02dcf617678a709acf1f85082a5ee66df369d3d
DHaylock <david.haylock@watershed.co.uk>
2018-02-01 15:20:05 +0000

    - Removed Back button

--- OhEuropa/OhEuropa/View Controllers/OEMapViewController.swift
+++ OhEuropa/OhEuropa/View Controllers/OEMapViewController.swift
@@ -57,33 +57,12 @@ class OEMapViewController: UIViewController, CLLocationManagerDelegate {
     }
 	
 	///------------------------------------------------------------------------------------------
-	/// View Did Layout Subviews
-	///------------------------------------------------------------------------------------------
-	override func viewDidLayoutSubviews() {
-		let backButton = UIButton(frame: CGRect(x: self.view.bounds.width-(14+40), y: self.view.bounds.height-(14+40), width: 40, height: 40))
-		backButton.setImage(UIImage.fontAwesomeIcon(name: .compass, textColor: DEFAULT_COLOR_OPPOSED, size: CGSize(width: 25, height: 25), backgroundColor: UIColor.clear), for: UIControlState.normal)
-		
-		backButton.addTarget(self, action: #selector(dismissViewController), for: .touchUpInside)
-		backButton.layer.cornerRadius = 0.5 * backButton.bounds.size.width
-		backButton.backgroundColor = DEFAULT_COLOR
-		self.view.addSubview(backButton)
-	}
-	
-	///------------------------------------------------------------------------------------------
 	/// Load The View
 	///------------------------------------------------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-	
-    ///------------------------------------------------------------------------------------------
-    /// Dismiss the View Controller
-    ///------------------------------------------------------------------------------------------
-	@objc func dismissViewController() {
-		print("Dismissing Popover")
-		self.dismiss(animated: true, completion: nil)
-	}
 
 	///------------------------------------------------------------------------------------------
 	/// Request Permission

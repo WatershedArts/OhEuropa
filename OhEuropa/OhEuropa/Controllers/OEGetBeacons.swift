e0420711ec1f1020ba89ef16e62f0bad3ecf65be
DHaylock <david.haylock@watershed.co.uk>
2018-02-02 15:34:37 +0000

    - Fixed Issue with Local Storage taking precedence over the Remote server. Ie the Beacons wouldn't return from the server :(

--- OhEuropa/OhEuropa/Controllers/OEGetBeacons.swift
+++ OhEuropa/OhEuropa/Controllers/OEGetBeacons.swift
@@ -18,62 +18,6 @@ class OEGetBeacons  {
 	
 	lazy var dataStack: DataStack = DataStack(modelName: "Beacons")
 	var items = [NSManagedObject]()
-
-	///-----------------------------------------------------------------------------
-	/// Get Data From The Server
-	///
-	///-----------------------------------------------------------------------------
-	func getDataFromRemoteServer() -> [OEMapBeacon] {
-		// Empty Container
-		var beacons = [OEMapBeacon]()
-		
-		// Get Request
-		Alamofire.request("https://www.davidhaylock.co.uk/oheuropa/getdata.php?getplaces")
-			.responseJSON { response in
-				
-				if response.error != nil || response.response?.statusCode != 200 {
-					print("GETLOCATIONS Bad Request")
-					if response.error != nil {
-						print("GETLOCATIONS: Failed to Get a Response")
-						print(response.error?.localizedDescription)
-						print(response.error!)
-					}
-				}
-				
-				if let data = response.result.value {
-					print("GETLOCATIONS: Got Return Data")
-					self.json = JSON(data)
-
-					print(self.json)
-					// If we get data from the server update the core data for backup incase of connection issues.
-					if let newdata = self.json["data"].arrayObject as? [[String:Any]] {
-						self.dataStack.sync(newdata, inEntityNamed: "Beacons") { error in
-							print("Updating the Core Data Storage")
-						}
-					}
-					
-					// Loop throught the return and parse the beacons
-					if let locationArray = self.json["data"].array {
-						for location in locationArray {
-							let loc = OEMapBeacon(
-								centercoordinate: CLLocationCoordinate2D(
-									latitude: Double(location["lat"].string!)!,
-									longitude: Double(location["lng"].string!)!),
-								centerradius: Double(location["centerradius"].string!)!,
-								innerradius: Double(location["innerradius"].string!)!,
-								outerradius: Double(location["outerradius"].string!)!,
-								datecreated: location["datecreated"].string!,
-								name: location["name"].string!,
-								nearbys: Int(location["nearbys"].string!)!,
-								placeid: location["placeid"].string!,
-								radioplays: Int(location["radioplays"].string!)!)
-							beacons.append(loc)
-						}
-					}
-				}
-		}
-		return beacons
-	}
 	
 	///-----------------------------------------------------------------------------
 	/// Get data from the Core Data Solutions
@@ -107,24 +51,63 @@ class OEGetBeacons  {
 		}
 		return beacons
 	}
-	
 
 	///-----------------------------------------------------------------------------
 	/// Initializer
+	/// We need to prioritize the Capture from the Server over the Local Storage
+	/// 
 	///
 	/// - Parameter completion: return values
 	///-----------------------------------------------------------------------------
 	init(_ completion: @escaping ([OEMapBeacon]) -> Void) {
-		
-		// Prioritize data from the Remote Server
-		var beacons: [OEMapBeacon] = getDataFromRemoteServer()
-		
-		if beacons.count > 0 {
-			completion(beacons)
-		}
-		else {
-			beacons = getBeaconsFromLocalServer()
-			completion(beacons)
+
+		var beacons = [OEMapBeacon]()
+
+		// Get Request
+		Alamofire.request("https://www.davidhaylock.co.uk/oheuropa/getdata.php?getplaces")
+			.responseJSON { response in
+
+				if response.error != nil || response.response?.statusCode != 200 {
+					print("GETLOCATIONS Bad Request")
+					if response.error != nil {
+						print("GETLOCATIONS: Failed to Get a Response")
+						print(response.error?.localizedDescription)
+						print(response.error!)
+					}
+					completion(self.getBeaconsFromLocalServer())
+				}
+
+				if let data = response.result.value {
+					print("GETLOCATIONS: Got Return Data")
+					self.json = JSON(data)
+
+					// If we get data from the server update the core data for backup incase of connection issues.
+					if let newdata = self.json["data"].arrayObject as? [[String:Any]] {
+						self.dataStack.sync(newdata, inEntityNamed: "Beacons") { error in
+							print("Updating the Core Data Storage")
+						}
+					}
+
+					// Loop throught the return and parse the beacons
+					if let locationArray = self.json["data"].array {
+						for location in locationArray {
+							let loc = OEMapBeacon(
+								centercoordinate: CLLocationCoordinate2D(
+									latitude: Double(location["lat"].string!)!,
+									longitude: Double(location["lng"].string!)!),
+								centerradius: Double(location["centerradius"].string!)!,
+								innerradius: Double(location["innerradius"].string!)!,
+								outerradius: Double(location["outerradius"].string!)!,
+								datecreated: location["datecreated"].string!,
+								name: location["name"].string!,
+								nearbys: Int(location["nearbys"].string!)!,
+								placeid: location["placeid"].string!,
+								radioplays: Int(location["radioplays"].string!)!)
+							beacons.append(loc)
+						}
+					}
+					completion(beacons)
+				}
 		}
 	}
 };

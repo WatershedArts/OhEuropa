4f5561358ed4d553de48ac000fefa79020ce11b6
DHaylock <david.haylock@watershed.co.uk>
2018-02-02 13:25:46 +0000

    - Removed UIExtension

--- OhEuropa/OhEuropa/extensions.swift
+++ OhEuropa/OhEuropa/extensions.swift
@@ -38,32 +38,27 @@ extension NSNotification.Name {
 
 extension UIImage {
 	
-	/// Returns a image that fills in newSize
-	func resizedImage(newSize: CGSize) -> UIImage {
-		// Guard newSize is different
-		guard self.size != newSize else { return self }
+	func maskWithColor(color: UIColor) -> UIImage? {
+		let maskImage = cgImage!
 		
-		UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
-		self.draw(in: CGRect(x: 0.0, y: 0.0, width: newSize.width, height: newSize.height))
-		let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-		UIGraphicsEndImageContext()
-		return newImage
-	}
-	
-	/// Returns a resized image that fits in rectSize, keeping it's aspect ratio
-	/// Note that the new image size is not rectSize, but within it.
-	func resizedImageWithinRect(rectSize: CGSize) -> UIImage {
-		let widthFactor = size.width / rectSize.width
-		let heightFactor = size.height / rectSize.height
+		let width = size.width
+		let height = size.height
+		let bounds = CGRect(x: 0, y: 0, width: width, height: height)
 		
-		var resizeFactor = widthFactor
-		if size.height > size.width {
-			resizeFactor = heightFactor
+		let colorSpace = CGColorSpaceCreateDeviceRGB()
+		let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+		let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+		
+		context.clip(to: bounds, mask: maskImage)
+		context.setFillColor(color.cgColor)
+		context.fill(bounds)
+		
+		if let cgImage = context.makeImage() {
+			let coloredImage = UIImage(cgImage: cgImage)
+			return coloredImage
+		} else {
+			return nil
 		}
-		
-		let newSize = CGSize(width: size.width/resizeFactor, height: size.height/resizeFactor)
-		let resized = resizedImage(newSize: newSize)
-		return resized
 	}
 	
 }
